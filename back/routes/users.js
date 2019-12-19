d1aebd7e2be81e97a3c16f45447c7b1d6e11c802
Sebastian Crossa <crossasebastian@gmail.com>
2019-12-19 01:40:03 -0700

    Basic version of user creation through the route implemented

--- back/routes/users.js
+++ back/routes/users.js
@@ -1,7 +1,8 @@
 // Libraries
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
 
@@ -20,7 +21,7 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req); // Runs all of the checks specified above and sees if any of the passed variables in the request has any errors
 
     // If there are errors on the validation, send a 400 status, alongside the error
@@ -28,7 +29,38 @@ router.post(
       return res.status(400).json({ errors: errors.array() }); // 400 - Bad request
     }
 
-    res.send(req.body);
+    const { name, email, password } = req.body;
+
+    try {
+      // Check if a user already exitst with the passed email
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res
+          .status(400)
+          .json({ msg: "A user already exitst with the passed email" });
+      } else {
+        // Create a new instance of the user with the passed data
+        user = new User({
+          name,
+          email,
+          password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send("User created!");
+      }
+    } catch (err) {
+      console.error(err.message);
+
+      res.status(500).json({ msg: "Server error" });
+    }
+
+    res.send("User created!");
   }
 );
 
